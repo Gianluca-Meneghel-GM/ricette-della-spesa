3402d830ded2ca9a0208523b7590ab54be1219ea
Gianluca Meneghel <70335590+Gianluca-Meneghel-GM@users.noreply.github.com>
2026-02-26 08:59:31 +0100

    Update page.tsx

--- app/weeks/[id]/shopping/page.tsx
+++ app/weeks/[id]/shopping/page.tsx
@@ -79,7 +79,7 @@ async function generateShoppingList() {
 setItems([{
           name: JSON.stringify(ingredientMap),
           total_quantity: 0,
-          unit: ri.unit,
+          unit: 2,
           checked: false
         }])
 
